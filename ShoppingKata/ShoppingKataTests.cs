50850717c9932368748b99874acf37d89d793c17
Iain Mitchell <iain.mitchell@codecomputerlove.com>
2014-03-21 08:50:29 +0000

    end of first session

--- ShoppingKata/ShoppingKataTests.cs
+++ ShoppingKata/ShoppingKataTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace ShoppingKata
 {
@@ -6,9 +7,42 @@ namespace ShoppingKata
     public class ShoppingKataTests
     {
         [Test]
-        public void Test()
+        public void WhenIScanOneAThenTotalIs50()
         {
-            Assert.That(true, Is.True);
+            var shoppingTil = new ShoppingTil();
+            shoppingTil.Scan("A");
+            int total = shoppingTil.Total();
+            Assert.That(total, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void WhenIScanOneBThenTotalIs30()
+        {
+            var shoppingTil = new ShoppingTil();
+            shoppingTil.Scan("B");
+            int total = shoppingTil.Total();
+            Assert.That(total, Is.EqualTo(30));
+        }
+    }
+
+    public class ShoppingTil
+    {
+        Dictionary<string,int> priceList = new Dictionary<string, int>
+            {
+                { "A", 50 },
+                { "B", 30 }
+            };
+
+        private int total;
+
+        public void Scan(string sku)
+        {
+            total = priceList[sku];
+        }
+
+        public int Total()
+        {
+            return total;
         }
     }
 }
