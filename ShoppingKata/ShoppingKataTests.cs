1210e6d83fdd0d8a1a43e9ccfa69d11cc0b084f7
Iain Mitchell <iain.mitchell@codecomputerlove.com>
2014-04-04 16:21:12 +0100

    finished third session

--- ShoppingKata/ShoppingKataTests.cs
+++ ShoppingKata/ShoppingKataTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace ShoppingKata
 {
@@ -6,5 +7,48 @@ namespace ShoppingKata
     public class ShoppingKataTests
     {
 
+        [Test]
+        public void WhenIScanOneATheTotalShouldReturn50()
+        {
+            const string skuOfA = "A";
+            const int priceOfA = 50;
+            var checkout = new Checkout(priceOfA);
+            checkout.Scan(skuOfA);
+            object value = checkout.Total();
+            Assert.That(value, Is.EqualTo(priceOfA));
+        }
+
+        [Test]
+        public void WhenIScanOneBTheTotalShouldReturn30()
+        {
+            const string skuOfB = "B";
+            const int priceOfB = 30;
+            var checkout = new Checkout(50);
+            checkout.Scan(skuOfB);
+            object value = checkout.Total();
+            Assert.That(value, Is.EqualTo(priceOfB));
+        }
+    }
+
+    public class Checkout
+    {
+        private readonly int _priceOfA;
+        private int total;
+
+        public Checkout(int priceOfA)
+        {
+            _priceOfA = priceOfA;
+        }
+
+        public void Scan(string sku)
+        {
+            if (sku.Equals("A")) total = _priceOfA;
+            else total = 30;
+        }
+
+        public int Total()
+        {
+            return total;
+        }
     }
 }
